1e0379b97f2e28b2d4bb695a0f38f838dfba3ae8
David Geldreich <david.geldreich@free.fr>
2024-03-23 18:22:58 +0100

    include/link python module with c10/torch

--- rocm/mamba_python.cpp
+++ rocm/mamba_python.cpp
@@ -4,11 +4,28 @@ namespace py = pybind11;
 #include <iostream>
 using namespace std;
 
+#include <hip/hip_runtime.h>
+#include <torch/extension.h>
+
+#include <c10/util/complex.h>  // For scalar_value_type
+using complex_t = c10::complex<float>;
+
+#include "selective_scan.h"
+
+template<typename input_t, typename weight_t>
+void selective_scan_fwd_cuda(SSMParamsBase& params, hipStream_t stream);
+
 void test()
 {
-    cout << "Hello World" << endl;
+    cout << "Test" << endl;
+    complex_t c;
+    at::Tensor a;
+    at::Tensor b;
+    cout << a.stride(0) << endl;
 }
 
-PYBIND11_MODULE(mamba, m) {
-    m.doc() = "Mamba for ROCm module";
+PYBIND11_MODULE(mamba_ssm, m) {
+    m.doc() = "ROCm Mamba module";
+//    m.def("fwd", &selective_scan_fwd, "Selective scan forward");
+    m.def("test", &test, "Test");
 }
